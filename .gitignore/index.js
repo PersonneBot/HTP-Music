a2ede4f05a6cee4db627796a0d5e06e30cc9cfca
PersonneBot <58113393+PersonneBot@users.noreply.github.com>
2021-01-16 11:28:24 +0100

    Add files via upload

---
+++ .gitignore/index.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = ":";
+client.login(process.env.TOKEN);
+const fs = require('fs');
+
+
+//on ready
+client.on("ready", () => {
+  console.log("Working")
+  client.user.setActivity({
+    type: "LISTENING",
+    name: ":help"
+});
+});
